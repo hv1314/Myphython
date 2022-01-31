fbc23b455b58055b0f27d10c712c1b232b70af50
venkat <venghada@gmail.com>
2022-01-31 09:08:32 +0530

    amstrong last

--- amstrong.py
+++ amstrong.py
@@ -1,1 +1,20 @@
-#commentssss
+#in all languages armstrong number has the same method as i know:
+
+
+
+num = int(input("Enter a number"))
+sum = 0
+temp=num
+while temp>0 :
+
+        r = temp%10
+        sum += r*r*r
+        temp //= 10
+
+if num == sum:
+        print(num,"Is a amstrong number")
+else:
+        print(num,"Is not a amtrong number")
+
+
+
